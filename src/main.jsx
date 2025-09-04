474cc0ce55eed63a7c0641c24db3e02c6330f658
Saif Shalan <safensound95@gmail.com>
2025-09-04 13:29:04 +0300

    feat: add filtering of notes by importance

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers(
+  {
+    notes:noteReducer,
+    filter:filterReducer,
+  }
+)
+
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
+
