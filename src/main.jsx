86db96899789d0c132429acee1e4eb5bacf6ad22
Saif Shalan <safensound95@gmail.com>
2025-09-07 20:13:54 +0300

    refactor: migrate reducers to Redux Toolkit (createSlice, configureStore)

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { combineReducers, createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers(
-  {
+const store = configureStore({
+  reducer: {
     notes:noteReducer,
     filter:filterReducer,
   }
-)
+})
 
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
