5455a0be3c6ba35c4483102fde7e0b27e8c3332b
Saif Shalan <safensound95@gmail.com>
2025-09-08 15:50:09 +0300

    feat: add async thunks and move store config to store.js

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -29,7 +29,23 @@
     }
   })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = (content) => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
+
 export default noteSlice.reducer
 
 
